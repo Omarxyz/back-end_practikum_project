144b8444918536b876a21d815243aeb943db5a6a
Omarxyz <kimsanov.omadbek@gmail.com>
2023-09-03 10:50:09 +0500

    yangiliklar sayti

---
+++ news_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import news_list, news_detail, indexPageView, ContactPageView, HomePageView, \
+    LocalNewsView, TechnologyNewsView, SportNewsView, ForeignNewsView
+
+urlpatterns = [
+    # path('', indexPageView, name='index_home_page'),
+    path('', HomePageView.as_view(), name='index_home_page'),
+    path('news/', news_list, name='all_news_list'),
+    path('news/<slug:news>/', news_detail, name='news_detail_page'),
+    path('contact-us/', ContactPageView.as_view(), name='contact_page'),
+    path('local/', LocalNewsView.as_view(), name='local_news_page'),
+    path('foreign/', ForeignNewsView.as_view(), name='foreign_news_page'),
+    path('technology/', TechnologyNewsView.as_view(), name='technology_news_page'),
+    path('sport/', SportNewsView.as_view(), name='sport_news_page'),
+]
